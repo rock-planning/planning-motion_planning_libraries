5d6a5f1e93ffa85c8fdf69d4419dacbacfb3ff7c
Stefan Haase <stefan.haase@dfki.de>
2014-02-21 12:26:19 +0100

    initial vizkit3d plugin code

---
+++ viz/vizkit_plugin.rb
@@ -0,0 +1,2 @@
+Vizkit::UiLoader.register_3d_plugin('WaypointVectorVisualization', 'global_path_planner', 'WaypointVectorVisualization')
+Vizkit::UiLoader.register_3d_plugin_for('WaypointVectorVisualization', "/std/vector<base/Waypoint>", :updateData )
