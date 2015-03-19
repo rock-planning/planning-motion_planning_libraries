b5386e972807c54eeb23de38cc3a1c040f55fb1d
Stefan Haase <stefan.haase@dfki.de>
2015-03-19 16:41:09 +0100

    Increases the driveabiliy of unknown areas in SBPL (close to obstacles).

--- src/sbpl/Sbpl.cpp
+++ src/sbpl/Sbpl.cpp
@@ -2,6 +2,8 @@
 
 #include <exception>
 
+#include <envire/operators/SimpleTraversability.hpp>
+
 #include <sbpl/sbpl_exception.h>
 
 namespace motion_planning_libraries
@@ -29,6 +31,7 @@ bool Sbpl::solve(double time) {
     try {
         // Current conclusion: Better not touch each planners epsilon.
         ret = mpSBPLPlanner->replan(time, &mSBPLWaypointIDs, &mLastSolutionCost);
+        mPathCost = mLastSolutionCost;
     } catch (...) {
         LOG_ERROR("Replanning failed");
         return false;
@@ -70,9 +73,27 @@ void Sbpl::createSBPLMap(envire::TraversabilityGrid* trav_grid,
     unsigned char* sbpl_map_p = mpSBPLMapData;
     uint8_t* stop_p = trav_data->origin() + trav_data->num_elements();
     
+    // The calculated costs of unknown areas (mean value of all grids)
+    // is set to a close-to-obstacle-cost to prevent using unknown areas.
+    std::vector <envire::TraversabilityClass > trav_classes = trav_grid->getTraversabilityClasses();
+    double highest_driveability = 0.0;
+    double last_highest_driveability = 0.0;
+    for(int i=0; i<trav_classes.size(); i++) {
+        double driveability = (trav_grid->getTraversabilityClass(i)).getDrivability();
+        if(driveability > highest_driveability) {
+            last_highest_driveability = highest_driveability; 
+            highest_driveability = driveability;
+        }
+    }
+    
+    LOG_WARN("Driveability of unknown areas is increased to %4.2f in SBPL", last_highest_driveability);
     for(uint8_t* p = trav_data->origin(); p < stop_p; p++, sbpl_map_p++) {
         uint8_t class_value = *p;
-        driveability = (trav_grid->getTraversabilityClass(class_value)).getDrivability();
+        if(class_value == envire::SimpleTraversability::CLASS_UNKNOWN) {
+            driveability = last_highest_driveability;
+        } else {
+            driveability = (trav_grid->getTraversabilityClass(class_value)).getDrivability();
+        }
         sbpl_cost = SBPL_MAX_COST - (int)(driveability * (double)SBPL_MAX_COST + 0.5) + 1.0;
         *sbpl_map_p = sbpl_cost;
     }
